f585dcaf8faebbca12457a56560d082a1ed6e918
Tejas810 <tejas8102001@gmail.com>
2024-02-05 17:03:12 +0530

    Added Logout functionality and done formattting for all pages

--- UserRegistration.php
+++ UserRegistration.php
@@ -1,135 +1,83 @@
 <!DOCTYPE html>
 <html>
-<head><meta charset="ISO-8859-1"><title> User Registration</title>
-<style>
-.register-box{
-  width:38%;   
-  height:34em; 
-  margin:auto; 
-  text-align:center; 
-  border-radius:0.5em; 
-  box-shadow:9px 9px 18px 9px rgba(0,0,0,0.1);	
-}
-h1{ 
-  padding-top:0.9em;      
-  text-align:center;
-  color:#013289; 
-}
-input[type=text],input[type=password],input[type=email]{
-  margin:0.5em; 
-  padding:0.7em 0em 0.7em 0.5em; 
-  width:65%; 
-  font-size:1.1em;
-  outline:none; 
-  border:none;
-  border-bottom:1px solid #013289;     
-  background-color:inherit;
-}
-input[type=text]:focus,input[type=password]:focus,input[type=email]:focus{
-  transition:0.3s; 
-  border-bottom:2px solid #013289; 
-}
-input::placeholder{ 
-  font-size:18px; 
-  color:#013289; 
-} 
-input[type=submit],input[type=submit]:focus{  
-  float:right;  
-  margin:1.8em 1em 0em 0em; 
-  padding:0.7em 2em;  
-  border:none;     
-  font-size:16px;
-  border-radius:5px;
-  cursor:pointer;
-  outline:none; 
-  color:#ffffff;
-  background-color:#4154f1;
-}
-a{
-  text-decoration:none;
-  color:#4154f1;
-}
-  
-</style>
+
+<head>
+  <meta charset="ISO-8859-1">
+  <title>User Registration</title>
+  <link rel="stylesheet" href="user-registration.css">
 </head>
-<body> <br>
-<div class="register-box"> 
-	<h1>Registration</h1>   
-	<p style="text-align:center;font-size:1.2em;">Already have an account ? <a href="user-login.php">Sign in </a></p> <br>    
-		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" autocomplete="off" onsubmit="setFlag()">			
-            <input type="text" id="firstname"  name="firstname" placeholder="Enter Your Firstname *" required><br>
-			<input type="text" id="lastname"  name="lastname" placeholder="Enter Your Lastname *" required><br> 
-            <input type="email" name="email" placeholder="Enter Your Mail Id *" required><br>
-			<input type="password" id="password" name="password" placeholder="Create Your Password *" required><br>
-			<label style="float:right;font-size:1.2em;margin:0px 1.5em 0em 0.7em;">Show Password</label>
-			<input type="checkbox" onclick="check_password()" style="float:right;"><br><br> 
-            <input type="text" id="flagKey" style="display:none;" name="flagKey" value="0">
-			<input type="submit" value="Create Account">
-		</form><br>
-</div> 
 
-<script>
-function check_password() {
-	  var x = document.getElementById("password");
-	  if (x.type == "password") {
-	    x.type = "text";
-	  } 
-	  else {
-	      x.type = "password";
-	  }
-	}
-function setFlag(){
-    document.getElementById('flagKey').value="1";
-}
-</script>
-<?php
-    if($_POST['flagKey']=="1")
-    {
-        $servername = "localhost";  
-        $username = "tejas";  
-        $password = "Tej@s";  
-        $database = "php";  
-        
-        $conn = new mysqli($servername, $username, $password, $database);
-        
-        
-        if ($conn->connect_error) {
-            die("Database Connection Error : " . $conn->connect_error);
-        }
-        
+<body>
+  <div class="register-box">
+    <h1>Registration</h1>
+    <p id="topLinks">Already have an account ? <a href="user-login.php">Sign in </a></p>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" autocomplete="off" onsubmit="setFlag()">
+      <input type="text" id="firstname" name="firstname" placeholder="Enter Your Firstname *" required>
+      <input type="text" id="lastname" name="lastname" placeholder="Enter Your Lastname *" required>
+      <input type="email" name="email" placeholder="Enter Your Mail Id *" required>
+      <input type="password" id="password" name="password" placeholder="Create Your Password *" required>
+      <label style="float:right;font-size:1.2em;margin:0px 1.5em 0em 0.7em;">Show Password</label>
+      <input type="checkbox" onclick="check_password()" style="float:right;">
+      <input type="text" id="flagKey" style="display:none;" name="flagKey" value="0">
+      <input type="submit" value="Create Account">
+    </form><br>
+  </div>
 
-        $firstname=$_POST['firstname'];
-        $lastname=$_POST['lastname'];
-        $email=$_POST['email'];
-        $password=$_POST['password'];
+  <script>
+    function check_password() {
+      var x = document.getElementById("password");
+      if (x.type == "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
 
+    function setFlag() {
+      document.getElementById('flagKey').value = "1";
+    }
+  </script>
 
+  <?php
+  include('database-connection.php');
 
-        $sql = "SELECT * FROM users WHERE email='$email'";
-        $result = $conn->query($sql);
+  if ($_POST['flagKey'] == "1") {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        if ($result->num_rows > 0) 
-        {
-            ?>
-                <script>alert('User Already Exist');</script>
-            <?php
-        }
-        else{
-            $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($sql);
 
-            $sql = "INSERT INTO users(firstname,lastname,email,password) VALUES ('$firstname', '$lastname','$email' ,'$encryptPassword')";
+    if ($result->num_rows > 0) {
+  ?>
+      <script>
+        alert('User Already Exist');
+      </script>
+      <?php
+    } else {
+      $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            if ($conn->query($sql) === TRUE) {
-                ?>
-                    <script>alert('User Registered Successfully');location.replace('user-login.php');</script>
-                <?php
-            } else {
-                ?>
-                    <script>alert('Error While Registering');</script>
-                <?php
-            }
-        }
-    }    
-?>
+      $sql = "INSERT INTO users(firstname,lastname,email,password) VALUES ('$firstname', '$lastname','$email' ,'$encryptPassword')";
+
+      if ($conn->query($sql) === TRUE) {
+      ?>
+        <script>
+          alert('User Registered Successfully');
+          location.replace('user-login.php');
+        </script>
+      <?php
+      } else {
+      ?>
+        <script>
+          alert('Error While Registering');
+        </script>
+  <?php
+      }
+    }
+  }
+  ?>
 </body>
+
 </html>
--- forgot-password.php
+++ forgot-password.php
@@ -1,121 +1,80 @@
 <!DOCTYPE html>
-<html><head>
-<meta charset="ISO-8859-1"><title>Forgot Password</title>
-<style>
-.VerifyBox{
-  margin:auto;
-  margin-top:2em; 
-  height:auto;  
-  width:35%;    	     
-  padding:1.2em 0em 1.5em 0em;   
-  text-align:center; 
-  border-radius:0.5em 0.5em 0em 0em; 
-  box-shadow:9px 9px 18px 9px rgba(0,0,0,0.1);
-}
-label{
-  float:left;
-  margin-left:2em;  
-  font-size:1.4em; 
-  font-weight:bold;  
-}
-input[type=text],input[type=password],input[type=email]{
-  width:80%;      
-  margin-top:0.8em; 
-  padding:0.2em 0.4em 0.1em 0em;  
-  font-size:20px;	
-  border:none;
-  border-bottom:1.5px solid #013289;  
-  outline:none; 
-  background-color:inherit;  
-}
-input[type=text]:focus,input[type=password]:focus,input[type=email]:focus{
-  border-bottom:2px solid #013289;
-  transition:0.5s;
-}
-input[type=submit]{ 
-  text-align:center;
-  padding:0.6em 1.8em;    
-  outline:none; 
-  border-radius:0.2em;
-  font-size:1.1em;
-  cursor:pointer;  
-  border:0.4px solid #000;
-  background-color:#fff;
-} 
-input[type=submit]:hover{ 
-  color:#fff; 
-  border:none; 
-  background-color:#4154f1;
-}
-</style>
+<html>
+
+<head>
+  <meta charset="ISO-8859-1">
+  <title>Forgot Password</title>
+  <link rel="stylesheet" href="forgot-password.css">
+</head>
+
 <body>
 
-<div class="VerifyBox">
-<h1 style="color:#4154f1;">Forgot Password</h1><br><br><br>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" autocomplete="off" onsubmit="setFlag()">
-	<label>Enter Your Mail Id</label>
-    <input type="email" name="email" required><br><br>
-    <label>Enter New Password </label>
-    <input type="password" id="password1" name="newpassword" required><br><br>
-    <label>Reenter New Password </label>
-    <input type="password" id="password2" onblur="CheckPasswordMatch()" required><br><br><br>
-    <input type="text" id="flagKey" style="display:none;" name="flagKey" value="0">
-	<input type="submit" id="submitBtn" value="Verify & Change Password" disabled ><br><br>
-</form>
-</div>
-<script>
-function setFlag(){
-    document.getElementById('flagKey').value="1";
-}
-function CheckPasswordMatch(){
-    if(document.getElementById("password1").value===document.getElementById("password2").value){
-        document.getElementById("submitBtn").disabled=false;
+  <div class="VerifyBox">
+    <h1 style="color:#4154f1;">Forgot Password</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" autocomplete="off" onsubmit="setFlag()">
+      <label>Enter Your Mail Id</label>
+      <input type="email" name="email" required>
+      <label>Enter New Password </label>
+      <input type="password" id="password1" name="newpassword" required>
+      <label>Reenter New Password </label>
+      <input type="password" id="password2" onblur="CheckPasswordMatch()" required>
+      <input type="text" id="flagKey" style="display:none;" name="flagKey" value="0">
+      <input type="submit" id="submitBtn" value="Verify & Change Password" disabled>
+    </form>
+  </div>
+  <script>
+    function setFlag() {
+      document.getElementById('flagKey').value = "1";
     }
-    else{
-      document.getElementById("submitBtn").disabled=true;
+
+    function CheckPasswordMatch() {
+      if (document.getElementById("password1").value === document.getElementById("password2").value) {
+        document.getElementById("submitBtn").disabled = false;
+      } else {
+        document.getElementById("submitBtn").disabled = true;
+      }
     }
-}
-</script>
-<?php
+  </script>
+  <?php
+  include('database-connection.php');
+  if ($_POST['flagKey'] == "1") {
 
-    if($_POST['flagKey']=="1")
-    {
-        $servername = "localhost";  
-        $username = "tejas";  
-        $password = "Tej@s";  
-        $database = "php";  
-        
-        $conn = new mysqli($servername, $username, $password, $database);
+    $email = $_POST['email'];
+    $password = $_POST['newpassword'];
 
-        $email = $_POST['email']; 
-        $password=$_POST['newpassword'];
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($sql);
 
-        $sql = "SELECT * FROM users WHERE email='$email'";
-        $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        if ($result->num_rows > 0) 
-        {
-          $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
-          
-          $sql = "UPDATE users SET password='$encryptPassword' where email='$email'";
+      $sql = "UPDATE users SET password='$encryptPassword' where email='$email'";
 
-            if ($conn->query($sql) === TRUE) {
-                ?>
-                  <script>alert("Password Changed Successfully !");location.replace('user-login.php');</script>
-                <?php
-            } else {
-              ?>
-                <script>alert("Unable to change password !");location.reload();</script>
-              <?php
-            }
-        } 
-        else {
-            ?>
-                <script>alert("User Doesn't Exist !");</script>
-            <?php
-        }
+      if ($conn->query($sql) === TRUE) {
+  ?>
+        <script>
+          alert("Password Changed Successfully !");
+          location.replace('user-login.php');
+        </script>
+      <?php
+      } else {
+      ?>
+        <script>
+          alert("Unable to change password !");
+          location.reload();
+        </script>
+      <?php
+      }
+    } else {
+      ?>
+      <script>
+        alert("User Doesn't Exist !");
+      </script>
+  <?php
     }
-?>
+  }
+  ?>
 
 </body>
+
 </html>
