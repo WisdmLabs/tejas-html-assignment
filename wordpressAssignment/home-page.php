1e684b2fe4baa343fccea808f942f392092d7ab4
Tejas810 <tejas8102001@gmail.com>
2024-03-05 17:46:33 +0530

    Main container image issue fixed

--- wordpressAssignment/home-page.php
+++ wordpressAssignment/home-page.php
@@ -26,26 +26,20 @@ if (have_posts()) {
     <div class="workshop-pricing-signup">
         <div class="pricing">
             <div class="price-member">
-                <div class="price">$200</div>
-                <br>
+                <div class="price">$200</div><br>
                 <div class="label">Member</div>
             </div>
             <div style="height: 6em;border-radius: 10px; border: 1.4px #434343 solid"></div>
             <div class="price-non-member">
-                <div class="price">$259</div>
-                <br>
+                <div class="price">$259</div><br>
                 <div class="label">Non-Member</div>
             </div>
         </div>
         <button class="btn-signup">Sign Up</button>
-    </div>
-    <br>
-    <br>
+    </div><br><br>
     <div class="signup">
         <a href="#" class="link-reduced-rate">Apply for Reduced-Rate Spot</a>
-    </div>
-    <br>
-    <br>
+    </div><br><br>
     <div class="workshop-details">
         <div class="workshop-dates" style="padding-right:18em;border-right: 1px solid #828282;">
             <p>
@@ -68,25 +62,7 @@ if (have_posts()) {
 <div class="workshop-info">
     <?php echo $page_content; ?>
 </div>
-<div class="carousel-container" style="background:url('https://s3-alpha-sig.figma.com/img/b048/4652/655f9e4eb6c24fa09a73b6125535e79a?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Tsg3MDWRHusiR-Fg3TDOgM~bJOkZWSCLYARJ1yerWnCTM4uFKGp8hkdWtp214T~W6W46wFELm2-n6Z9Q6ZHeDSRuI8SqE-R6H5rizELlwryYazIy9u5HVscN1tAkfOoRpzLWsvOo91WtFxtc5TpJ0mHAuW01jn148bY3o7vC1uWo3k4ED7uijAwbx0v7B0fruCFoKh~duC8mhdZAfoHvRCR2inffvOyBEBjk5xz6Unjuft0ZmcGHKiywhKikwaWUAPnCi19tpadDtll5jwyLTMr1FJvij3facHHxa4ekI1OElsezooF1mZckwRqLoISaUvHeh-kdZYmcSdbZQru4CA__');background-size: cover;background-repeat: no-repeat;background-position: center;overflow: hidden;">
-    <div class="carousel-slide" id="carouselSlide">
-        <div class="carousel-item">
-            <button class="carousel-button" id="prevBtn">&#10094;</button>
-            <div class="testimonial-text-container">
-                <div class="testimonial-quote quote-left">“</div>
-                <p class="testimonial-text"> &nbsp;&nbsp;Erin was a wonderful teacher. She was personable, kind, organized, <br>and great with facilitating group discussion. <br>
-                    <br>
-                    <span class="author">Katy L.</span>
-                </p>
-                <div class="testimonial-quote quote-right">”</div>
-            </div>
-            <button class="carousel-button" id="nextBtn">&#10095;</button>
-        </div>
-    </div>
-</div>
-<br>
-<br>
-<br>
+
 <section class="similar-classes-container">
     <h2 class="section-title">Similar Classes</h2>
     <div class="classes-list">
@@ -104,7 +80,6 @@ if (have_posts()) {
         </article>
     </div>
 </section>
-<br><br><br><br>
 <div class="remote-classes-container">
     <h2 class="section-heading">About the Remote Classes</h2>
     <p class="section-content">You will be able to participate in live class meetings via Zoom videoconference. To attend classes, you'll need a phone, tablet or computer and access to the internet. You can participate in the class from wherever you'd like, whether on your living room couch or in your office. Before your class meets, you'll receive an email from The Proprietary with more information about Zoom and your remote class. If you have any questions about remote learning, please don't hesitate to reach out to us at <span style="color:#00ACB4;">hello@wisdmlabs.com.</span>
@@ -115,7 +90,6 @@ if (have_posts()) {
         <a href="#" class="section-link">Questions? See FAQ <span class="fa fa-arrow-right"></span>
         </a>
     </div>
-    <br>
 </div>
 <hr style="width:100%;height: 2px;background-color:#434343;">
 <div class="cancellation-policy-container">
@@ -140,7 +114,6 @@ if (have_posts()) {
             <p class="rule-description">The Proprietary cannot offer refunds, credits, or makeup sessions for classes a student might miss.</p>
         </div>
     </div>
-    <br>
 </div>
 <?php
 get_footer();
