6c5b3a0ab99c67e565e09a4428c62996e6c6b0ae
Tejas810 <tejas8102001@gmail.com>
2024-03-07 16:38:27 +0530

    Font families add and minor changes in structure

---
+++ Wordpress Assignment2/functions.php
@@ -0,0 +1,19 @@
+<?php
+function add_fonts()
+{
+    wp_register_style("font1", "https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap");
+    wp_enqueue_style("font1");
+
+    wp_register_style("font2", "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
+    wp_enqueue_style("font2");
+
+    wp_register_style("font3", "https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+    wp_enqueue_style("font3");
+}
+function add_icons()
+{
+    wp_register_style("icons", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+    wp_enqueue_style("icons");
+}
+add_action('wp_enqueue_scripts', 'add_fonts');
+add_action('wp_enqueue_scripts', 'add_icons');
