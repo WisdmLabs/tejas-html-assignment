3144af077a977205bf59ee3c3ea3661eec279534
Tejas810 <tejas8102001@gmail.com>
2024-03-20 14:21:44 +0530

    Fixed function names according to standards

--- Wordpress Assignment2/functions.php
+++ Wordpress Assignment2/functions.php
@@ -1,5 +1,8 @@
 <?php
-function add_fonts()
+/*
+This function is used to register and enqueue custom fonts for the page
+*/
+function wdm_add_fonts()
 {
     wp_register_style("font1", "https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap");
     wp_enqueue_style("font1");
@@ -10,10 +13,13 @@ function add_fonts()
     wp_register_style("font3", "https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
     wp_enqueue_style("font3");
 }
-function add_icons()
+/*
+This function is used to add custom icons for the page
+*/
+function wdm_add_icons()
 {
     wp_register_style("icons", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style("icons");
 }
-add_action('wp_enqueue_scripts', 'add_fonts');
-add_action('wp_enqueue_scripts', 'add_icons');
+add_action('wp_enqueue_scripts', 'wdm_add_fonts');
+add_action('wp_enqueue_scripts', 'wdm_add_icons');
